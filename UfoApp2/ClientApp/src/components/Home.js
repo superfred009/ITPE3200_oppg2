475686397b108633ca4c278aedcdd1dc7eaa0f0e
PedramZi <91465878+PedramZi@users.noreply.github.com>
2022-11-18 18:07:12 +0100

    uncomplete

--- UfoApp2/ClientApp/src/components/Home.js
+++ UfoApp2/ClientApp/src/components/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { FetchUfoData } from './FetchUfoData';
 import { UfoForm } from './UfoForm';
-import { Innloggingsskjema } from './Innloggingsskjema';
 import './custom.css';
 
 export class Home extends Component {
