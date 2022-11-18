46e90081d78f242f121d475cd3ec59947c00a992
PedramZi <91465878+PedramZi@users.noreply.github.com>
2022-11-18 12:19:01 +0100

    innlogging html

--- UfoApp2/ClientApp/src/components/innloggingsskjema.js
+++ UfoApp2/ClientApp/src/components/innloggingsskjema.js
@@ -9,20 +9,19 @@ export class Innloggingsskjema extends Component {
                 <h1>Innlogging</h1>
                 <div class="md-form">
                     <i class="fas fa-envelope prefix"></i>
-                    <input type="text" name="email" class="form-control">
+                    <input type="text" name="email" class="form-control"/>
                         <label for="orangeForm-email">Din epost</label>
                 </div>
 
                 <div class="md-form">
                     <i class="fas fa-lock prefix"></i>
-                    <input type="password" name="password" class="form-control">
+                    <input type="password" name="password" class="form-control"/>
                         <label for="orangeForm-pass">Ditt passord</label>
                 </div>
 
                 <div class="text-center">
-                    <input class="btn btn-info btn-rounded mt-5" type="submit" name="submit" value="Logg inn">
+                    <input class="btn btn-info btn-rounded mt-5" type="submit" name="submit" value="Logg inn"/>
                 </div>
-                <p class="text-center"><a class="btn btn-primary toggleForms">Registrering</a></p>
             </div>
         );
     }
