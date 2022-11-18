797aef6ef03b34cc07da298481df158ad097aa7a
aleksanderaraya <s356403@oslomet.no>
2022-11-18 13:07:30 +0100

    lagt til innlogging html

--- UfoApp2/ClientApp/src/components/Home.js
+++ UfoApp2/ClientApp/src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { FetchUfoData } from './FetchUfoData';
 import { UfoForm } from './UfoForm';
+import { Innlogging } from './Innlogging';
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -10,7 +11,8 @@ export class Home extends Component {
       <div>
          <h1>UFO observasjoner</h1>
             <FetchUfoData />
-            <innloggingsskjema />
+           
+            <Innlogging />
       </div>
     );
   }
