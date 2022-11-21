c493ae28fd72afbde9d9d0e51a687f8c6ff00d16
fredrikneby <s351889@oslomet.no>
2022-11-21 14:04:19 +0100

    fixed loading correct object on explore

--- UfoApp2/ClientApp/src/components/ExploreUfo.js
+++ UfoApp2/ClientApp/src/components/ExploreUfo.js
@@ -1,26 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "../custom.css";
 
 export class ExploreUfo extends Component {
-    static displayName = ExploreUfo.name;
+  static displayName = ExploreUfo.name;
 
-    render() {
-        return (
-            <div>
-                <h1>UFO tittel</h1>
-                <p>Denne siden er work in progress, trenger fetching fra databasen</p>
+  constructor(props) {
+    super(props);
+    this.state = { observasjon: [], loading: true };
+  }
 
-                <iframe
-                    className='map'
-                    width='600' 
-                    height='400'
-                    style={{border: 'none'}}
-                    loading='lazy'
-                    allowFullScreen 
-                    referrerPolicy = 'no - referrer - when - downgrade'
-                    src = {'https://www.google.com/maps/embed/v1/place?key=AIzaSyAh3r_xSPcltV8W1V6I6IGmuJQsyaDREnU&q='+'Oslo'}
-                 ></iframe >
-            </div>
-        );
-    }
+  componentDidMount() {
+    this.populateUfoData();
+  }
+
+  static renderExplored(observasjon) {
+    return (
+      <div>
+        <h1>{observasjon.tittel}</h1>
+        <p>Dato: {observasjon.dato}</p>
+        <p>Sted: {observasjon.sted}</p>
+        <p>Beskrivelse: {observasjon.beskrivelse}</p>
+        <iframe
+          className="map"
+          width="600"
+          height="400"
+          style={{ border: "none" }}
+          loading="lazy"
+          allowFullScreen
+          referrerPolicy="no - referrer - when - downgrade"
+          src={
+            "https://www.google.com/maps/embed/v1/place?key=AIzaSyAh3r_xSPcltV8W1V6I6IGmuJQsyaDREnU&q=" +
+            observasjon.sted
+          }
+        ></iframe>
+      </div>
+    );
+  }
+
+  render() {
+    let contents = this.state.loading ? (
+      <p>
+        <em>Loading ...</em>
+      </p>
+    ) : (
+      ExploreUfo.renderExplored(this.state.observasjon)
+    );
+
+    return <div>{contents}</div>;
+  }
+
+  async populateUfoData() {
+    const response = await fetch("ufo/hentEn?id=" + this.props.match.params.id);
+    console.log("response: ", response);
+    const data = await response.json();
+    console.log("data: ", data);
+    this.setState({ observasjon: data, loading: false });
+  }
 }
